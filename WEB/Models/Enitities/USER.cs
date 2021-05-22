a4b75f12dd16f725b4431bd163c715d132f9bcee
Dương Nguyễn <ntduong1292@gmail.com>
2021-05-22 21:25:39 +0700

    update TakaDB.cs

--- WEB/Models/Enitities/USER.cs
+++ WEB/Models/Enitities/USER.cs
@@ -31,10 +31,7 @@ namespace taka.Models.Enitities
 
         [StringLength(50)]
         public string FULLNAME { get; set; }
-        [StringLength(10)]
-        public string GENDER { get; set; }
-        [StringLength(50)]
-        public string BIRTHDAY { get; set; }
+
         public int? isBAN { get; set; }
 
         [StringLength(50)]
@@ -42,6 +39,12 @@ namespace taka.Models.Enitities
 
         [StringLength(250)]
         public string GOOGLE_ID { get; set; }
+
+        [StringLength(10)]
+        public string GENDER { get; set; }
+
+        [StringLength(50)]
+        public string BIRTHDAY { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ADDRESS> ADDRESSes { get; set; }
